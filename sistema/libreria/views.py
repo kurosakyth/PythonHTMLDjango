d71757327612c1613c93e4428a5e9d68300c8be0
aasan <aasantamariac@hotmail.es>
2022-04-20 11:37:56 -0600

    sistema local web agregado

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -1,3 +1,7 @@
+from ast import Return
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def inicio(request):
+    return HttpResponse("<h1>ESA PUTA MISSFORTUNE QUE VERGA HACE</h1>")
