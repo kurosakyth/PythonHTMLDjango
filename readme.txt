a867a300e7a23ccd2b10d11ff4dbfd8a635b24a9
aasan <aasantamariac@hotmail.es>
2022-04-21 13:25:31 -0600

    agregando tabla a la base de datos

--- readme.txt
+++ readme.txt
@@ -1,4 +1,11 @@
 python
 Django
-pip3 install PyMySQL
-pip3 install pillow
+
+-base de datos:
+Install XAMPP.
+Run APACHE and MySQL.
+Run on the terminal: pip3 install PyMySQL
+Run on the terminal: pip3 install pillow
+
+Una vez la estructura de models.py de la base de datos esté hecha, se corre: python manage.py makemigrations
+luego se hace la migración a la base con: python manage.py migrate
--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#captura la estructura la tabla libros en este caso
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    imagen = models.ImageField(upload_to = 'images/', verbose_name='Imagen',null = True)
+    descipcion = models.TextField(verbose_name='Descripcion', null = True)
