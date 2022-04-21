ef996c477fe31db14a0c8ed99deb77c2ee2c52f5
aasan <aasantamariac@hotmail.es>
2022-04-21 13:51:08 -0600

    agregando método delete en base de datos de admin

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -1,3 +1,4 @@
+from ast import Delete
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,11 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=100, verbose_name='Título')
     imagen = models.ImageField(upload_to = 'images/', verbose_name='Imagen',null = True)
     descipcion = models.TextField(verbose_name='Descripción', null = True)
+
+    def __str__(self):
+        fila = "Título: " + self.titulo + " - " + "Descripción: " + self.descipcion
+        return fila
+    
+    def delete(self, using = None, keep_parents = False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
