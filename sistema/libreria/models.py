0bf453b49b48b45ca9522e2138ee0c149c31427c
aasan <aasantamariac@hotmail.es>
2022-04-21 22:05:47 -0600

    trayendo datos a la vista editar y cambiando en la base de datos

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -7,7 +7,7 @@ class Libro(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100, verbose_name='Título')
     imagen = models.ImageField(upload_to = 'images/', verbose_name='Imagen',null = True)
-    descipcion = models.TextField(verbose_name='Descripción', null = True)
+    descripcion = models.TextField(verbose_name='Descripción', null = True)
 
     def __str__(self):
         fila = "Título: " + self.titulo + " - " + "Descripción: " + self.descipcion
