7c430104711b94f23d3e6b7f827814f37cac3833
aasan <aasantamariac@hotmail.es>
2022-04-21 21:47:41 -0600

    agregando vista de datos a editar

--- sistema/libreria/urls.py
+++ sistema/libreria/urls.py
@@ -7,12 +7,11 @@ from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('', views.inicio, name='inicio'),    
     path('nosotros', views.nosotros, name='nosotros'),
-    
     path('libros', views.libros, name='libros'),
     path('libros/crear', views.crear, name='crear'),
     path('libros/editar', views.editar, name='editar'),
     path('eliminar/<int:id>', views.eliminar, name='eliminar'),
-
+    path('libros/editar/<int:id>', views.editar, name='editar'),
 
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 #urls trae el html de views, entonces monta primero las views, para tener la dirección en la web.
